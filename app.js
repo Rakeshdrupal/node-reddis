38bcdeb593de4a0867f3954a7ba1e3db1da4f411
Rakesh Kumar <rakeshpriy143@gmail.com>
2017-08-22 14:37:26 +0530

    completed chapter 1

--- app.js
+++ app.js
@@ -4,40 +4,46 @@ const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const bodyParser = require('body-parser');
 const csrf = require('csurf');
+const flash = require('connect-flash');
 const RedisStore = require('connect-redis')(session);
 const app = express();
 const routes = require('./routes');
 const errorHandlers = require('./middleware/errorhandlers');
 const log = require('./middleware/log');
 const util = require('./middleware/utilities');
+const config = require('./config');
 app.set('view engine', 'ejs');
 app.set('view options', { defaultLayout: 'layout' });
 app.use(log.logger);
 app.use(partials());
 app.use(cookieParser('secret'));
 app.use(session({
-    secret: 'secret',
+    secret: config.secret,
     saveUninitialized: true,
     resave: true,
     store: new RedisStore(
-        { url: 'redis://localhost' })
+        { url: config.redisUrl })
 })
 );
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
+app.use(flash());
 app.use(csrf());
-//app.use(util.csrf());
+app.use(util.csrf);
+app.use(util.authenticated);
+app.use(util.templateRoutes);
 app.use(express.static(__dirname + '/static'));
-app.use(function (req, res, next) {
-    app.locals.token = req.csrfToken();
+// app.use(function (req, res, next) {
+//     res.locals.token = req.csrfToken();
 
     
-    next();
-});
+//     next();
+// });
 app.get('/', routes.index);
-app.get('/login', routes.login);
-app.post('/login', routes.loginProcess);
-app.get('/chat', routes.chat);
+app.get(config.routes.login, routes.login);
+app.post(config.routes.login, routes.loginProcess);
+app.get(config.routes.logout, routes.logOut);
+app.get('/chat',[util.requireAuthentication], routes.chat);
 app.use(errorHandlers.notFound);
-app.listen(3030);
+app.listen(config.port);
 console.log("App server running on port 3000");
--- middleware/utilities.js
+++ middleware/utilities.js
@@ -1,5 +1,40 @@
-module.export.csrf = function(req,res,next) {
-    console.log(req);
-    //res.locals.token = req.csrfToken();
-    //next();
+const config = require('../config');
+module.exports = {
+    csrf: function (req, res, next) {
+        res.locals.token = req.csrfToken();
+        next();
+    },
+    authenticated: function (req, res, next) {
+        res.locals.isAuthenticated = req.session.isAuthenticated;
+        if (req.session.isAuthenticated) {
+            res.locals.user = req.session.user;
+        }
+        next();
+    },
+    requireAuthentication: function (req, res, next) {
+        if (req.session.isAuthenticated) {
+            next();
+        } else {
+            res.redirect(config.routes.login);
+        }
+    },
+    auth: function (username, password, session) {
+        var isAuth = username === 'joshua' || username === 'brian';
+        if (isAuth) {
+            session.isAuthenticated = isAuth;
+            session.user = {
+                username: username
+            };
+        }
+        return isAuth;
+    },
+    logOut: function (session) {
+        session.isAuthenticated = false;
+        delete session.user;
+    },
+    templateRoutes: function(req, res, next) {
+        res.locals.routes = config.routes;
+        next();
+    }
+
 };
